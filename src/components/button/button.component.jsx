4b319979a14e33115867881b44890878c06d9335
Christopher Cargill <chriscargill2761@gmail.com>
2024-07-09 15:37:22 -0400

    create authentication page component alongside sign in form

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -9,8 +9,8 @@ const BUTTON_TYPE_CLASSES = {
 const Button = ({children, buttonType, ...otherProps}) => {
     return (
         <button
-        className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-        {...otherProps}
+            className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+            {...otherProps}
         >
         {children}
         </button>
---
+++ src/components/routes/authentication/authentication.component.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+    signInWithGooglePopup,
+    signInAuthUserWithEmailAndPassword,
+} from "../../../utils/firebase/firebase.utils";
+import Button from "../../button/button.component";
+import FormInput from "../../form-input/form-input.component";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
+import './authentication.styles.scss';
+
+const Authentication = () => {
+    const defaultFormFields = {
+        email: '',
+        password: '',
+    }
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {email, password} = formFields;
+
+    const signInGoogleUser = async () => {
+        await signInWithGooglePopup();
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormFields({...formFields, [name]: value});
+    }
+
+    const handleSubmit = async(event) => {
+        event.preventDefault(); //prevents any default beahavior of the form
+
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+        } catch(error) {
+            switch(error.code) {
+                case 'auth/wrong-password':
+                    alert("Incorrect password");
+                    break;
+                case 'auth/user-not-found':
+                    alert("No user associated with this email")
+                    break;
+                default:
+                    console.log(error);
+            }
+        }
+    }
+    
+    return (
+        <div className="sign-in-container">
+            <div>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={handleSubmit}>
+                    <FormInput
+                        label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
+
+                    <FormInput 
+                        label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
+
+                    <div className='sign-in-button-container'>
+                        <Button type="submit">Sign In </Button>
+                        <Button type="button" buttonType="google" onClick={signInGoogleUser}>Sign In With Google </Button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <SignUpForm/>
+            </div>
+        </div>
+    )
+}
+
+export default Authentication;
